bdbd041c2647c39da5c04aa013b24decf46ea73a
monmoono <56115746+monmoono@users.noreply.github.com>
2022-11-03 23:15:18 +0700

    Style index

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,36 +1,38 @@
 <template>
   <v-app>
-    <v-main>
-      <v-row justify="center" class="container1">
-        <v-col class="box1">
-          <h1>Meet our leadership</h1>
-          <p>
-            Libero fames augue nisl porttitor nisi,quis. Id ac elit odio vitae
-            elementum enim vitae ullamcorper suspendisse.Vivamus fringilla.
-          </p>
-        </v-col>
-        <v-col class="box2">
-          <v-list-item three-line v-for="(item, index) in items" :key="index">
-            <template>
-              <img
-                :src="item.avatar"
-                style="width: 75px; height: 75px; border-radius: 50%"
-              />
-            </template>
-            <v-list-item-content>
-              <h4>{{ item.first_name + " " + item.last_name }}</h4>
-              <h5 class="emailtext">{{ item.email }}</h5>
-            </v-list-item-content>
-          </v-list-item>
-        </v-col>
-      </v-row>
-      <!-- <v-container flat>
-       
-      </v-container> -->
+    <v-main class="com">
+      <v-container flat>
+        <v-row no-gutters class="container1">
+          <v-col class="box1">
+            <h1 class="text-h">Meet our leadership</h1>
+            <p class="textp">
+              Libero fames augue nisl porttitor nisi,quis. Id ac elit odio vitae
+              elementum enim vitae ullamcorper suspendisse.Vivamus fringilla.
+            </p>
+          </v-col>
+          <v-col class="box2 list-head">
+            <v-list-item
+              three-line
+              v-for="(item, index) in items"
+              :key="index"
+              class="list-list"
+            >
+              <template>
+                <img class="list-img" :src="item.avatar" />
+              </template>
+              <v-list-item-content>
+                <v-list-item-title class="list-name">{{
+                  item.first_name + " " + item.last_name
+                }}</v-list-item-title>
+                <v-list-item-subtitle class="list-email">{{
+                  item.email
+                }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
-    <!-- <v-card class="flex" flat tile>
-      <v-card-title> </v-card-title>
-    </v-card> -->
   </v-app>
 </template>
 
@@ -56,32 +58,53 @@ export default {
         };
         arr.push(obj);
       }
-      //this.datatable = arr;
       this.items = arr;
       console.log(arr);
     });
   },
-  methods: {},
 };
 </script>
 
 <style lang="sass">
 .box1
   grid-area: Box1
-  width: 32rem
 
 .box2
   display: grid
   grid-area: Box2
   grid-template-columns: auto auto
 
-
 .container1
   display: grid
-  grid-template-areas: "Box1 Box2 Box2 Box2 Box2"
+  grid-template-areas: "Box1 Box2 Box2"
   gap: 1rem
-  padding: 1rem
+  padding: 7% auto
 
-.emailtext
-  color: #3F51B5
+.textp
+  line-height: 1.5
+  padding-top: 1.5rem
+  font-size: 1.5rem
+
+.text-h
+  font-size: 3.5rem
+
+.list-name
+  padding-left: 15px
+  font-size: 1.5rem
+
+.list-email
+  padding-left: 15px
+  font-size: 1rem
+
+.list-head
+  margin-left: 10%
+
+
+.list-img
+  width: 85px
+  height: 85px
+  border-radius: 50%
+
+.com
+  margin-top: 8%
 </style>
